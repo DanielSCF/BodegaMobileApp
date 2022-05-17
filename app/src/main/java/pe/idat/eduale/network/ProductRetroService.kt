86c2e00a0c73a25d8ff9e4cf8e0a798ad78a10c5
DanielSCF <needdaniel@hotmail.com>
2022-05-17 18:27:11 -0500

    Retrofit get product by id and getting product for cart by user (not polished)

--- app/src/main/java/pe/idat/eduale/network/ProductRetroService.kt
+++ app/src/main/java/pe/idat/eduale/network/ProductRetroService.kt
@@ -12,4 +12,10 @@ interface ProductRetroService {
     @GET("productos/buscar/{nombre}")
     fun getProductByName(@Path("nombre") nombre:String): Call<List<ProductModel>>
 
+    @GET("productos/{productoid}")
+    fun getProductById(@Path("productoid") productoid:Int): Call<ProductModel>
+
+    @GET("productos/{usuarioid}/{productoid}")
+    fun userAddProductoCart(@Path("usuarioid") usuarioid:Int, @Path("productoid") productoid:Int):Call<List<ProductModel>>
+
 }
