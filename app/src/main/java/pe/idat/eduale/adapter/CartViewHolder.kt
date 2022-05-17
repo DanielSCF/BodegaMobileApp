7961f10dbb7254022070528dcf89f0a7db48b5f7
DanielSCF <needdaniel@hotmail.com>
2022-05-17 18:00:20 -0500

    Cart view holder initial information

--- app/src/main/java/pe/idat/eduale/adapter/CartViewHolder.kt
+++ app/src/main/java/pe/idat/eduale/adapter/CartViewHolder.kt
@@ -2,6 +2,7 @@ package pe.idat.eduale.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import pe.idat.eduale.databinding.ItemCartBinding
 import pe.idat.eduale.model.CartModel
 
@@ -9,9 +10,11 @@ class CartViewHolder (view:View): RecyclerView.ViewHolder(view){
     var binding = ItemCartBinding.bind(view)
 
     fun render(cartModel: CartModel){
+        //Product information
+        binding.txtNombreProducto.text = cartModel.product.nombre+ " - " + cartModel.product.descripcion
+        binding.txtPrecio.text = "S/." + cartModel.product.precioventa.toString()
 
 
-
-
+        Glide.with(binding.ImgProduct.context).load(cartModel.product.imagen).into(binding.ImgProduct)
     }
 }
--- app/src/main/java/pe/idat/eduale/model/CartModel.kt
+++ app/src/main/java/pe/idat/eduale/model/CartModel.kt
@@ -2,6 +2,7 @@ package pe.idat.eduale.model
 
 data class CartModel (
     val cantidad:Int,
+    val subtotal:Double?,
     val total:Double,
 
     val product:ProductModel
